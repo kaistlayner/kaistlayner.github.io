b49da6c2635ab84b8efe62960de8d9d1f231fa5a
kaistlayner <layner@kaist.ac.kr>
2024-03-10 18:45:46 +0900

    --fix

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: "https://kaistlayner.github.io/portfolio/",
+  reactStrictMode: true,
+};
 
 export default nextConfig;

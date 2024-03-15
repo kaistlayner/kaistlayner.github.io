4b7c260586259079533321d98c164d07c3396f6f
kaistlayner <layner@kaist.ac.kr>
2024-03-16 04:03:40 +0900

    skill 내용 간단히 추가

--- portfolio/constant/skill.ts
+++ portfolio/constant/skill.ts
@@ -1,4 +1,17 @@
 export const skill: Record<string, string> = {
-  react: "좋은 툴이죠",
-  nextjs: "좋은 툴이죠22",
+  reactJS:
+    "웹 개발을 위한 프론트엔드 라이브러리, 다양한 컴포넌트 생성 및 백엔드 api 연결",
+  css: "css scss styled-component",
+  nextJS: "ssr, seo",
+  typescript:
+    "타입이 틀리면 lint error가 발생하여 배포 전 실수 방지, 타입을 통한 코드 가이드 및 가독성 향상",
+  expressJS:
+    "기본적인 js로 백엔드 서버 구축, db 연결 및 관리, 프론트로 api 제공, 로그 수집",
+  knex: "migration up down",
+  nestJS: "express vs nest",
+  prisma: "schema file, code history",
+  graphql: "캐시닥 커뮤니티, client can select data",
+  mysql: "관계형 db",
+  redis: "잘 변하지 않는 아이콘, 주기적으로 변경되는 데이터 => api 호출 최소화",
+  aws: "프론트 amplify를 통한 배포, 백엔드 cloudWatch로 로그 확인, s3 이미지 관리, eb/ec2 를 통해 배포 상황 확인, 특정 배포로 되돌리기",
 };

1fc947cb0df686c4f7159b5d5791dfcb7c669ac8
kaistlayner <layner@kaist.ac.kr>
2024-03-16 10:01:49 +0900

    --fix

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from "next/app";
-import Layout from "../component/Layout";
 import { DefaultSeo } from "next-seo";
-import "../global.scss";
+import Layout from "../component/Layout";
 import { colors } from "../constant";
+import "../global.scss";
 
 const title = "조민준의 포트폴리오";
 const description = "카이스트 16학번 조민준의 포트폴리오입니다.";

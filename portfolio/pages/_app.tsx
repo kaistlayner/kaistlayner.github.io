578752494c07e06356c571aa8fb6251ef5e6d638
kaistlayner <layner@kaist.ac.kr>
2024-03-16 09:51:42 +0900

    후잉

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,10 +1,24 @@
 import { AppProps } from "next/app";
 import Layout from "../component/Layout";
+import { DefaultSeo } from "next-seo";
 import "../global.scss";
+
+const title = "조민준의 포트폴리오";
+const description = "카이스트 16학번 조민준의 포트폴리오입니다.";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo
+        title={title}
+        description={description}
+        openGraph={{
+          title,
+          description,
+          type: "website",
+          images: [{ url: "/work.jpg" }],
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   );
